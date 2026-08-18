2968e0d4f87239696e74ac6b06eb830742e3f39f
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-18 23:08:07 +0530

    alternative array type declaration

--- 01 - typescript-basics/arrays-objects.ts
+++ 01 - typescript-basics/arrays-objects.ts
@@ -4,3 +4,6 @@ let hobbies = ["sports", "coding"];
 
 let users: (string | number)[] = ["Amandeep", 36]; // This is valid because users can contain both strings and numbers.
 users.push("John");
+
+// Generics types allow you to create reusable components that work with a variety of data types while still maintaining type safety.
+const numbers: Array<number | string> = ["one", 2, 3];
