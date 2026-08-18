c782f87c6cf6d707f1016d4ba05bcb4d7aea2874
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-18 22:36:47 +0530

    assigning types of function parameters

--- 01 - typescript-basics/basics.ts
+++ 01 - typescript-basics/basics.ts
@@ -5,3 +5,11 @@ username = "Amandeep";
 // username = true; // Error: Type 'boolean' is not assignable to type 'string'.
 
 let age = 36;
+
+// Assigning types of function parameters
+function sum(a: number, b = 10) {
+  return a + b;
+}
+
+const result = sum(3);
+console.log(result); // Output: 13
