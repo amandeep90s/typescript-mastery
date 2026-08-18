92f3461a7583ac0ae9d810801d0e108734855462
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-18 23:14:44 +0530

    tuples type

--- 01 - typescript-basics/arrays-objects.ts
+++ 01 - typescript-basics/arrays-objects.ts
@@ -7,3 +7,6 @@ users.push("John");
 
 // Generics types allow you to create reusable components that work with a variety of data types while still maintaining type safety.
 const numbers: Array<number | string> = ["one", 2, 3];
+
+// Tuples are a special type of array that allows you to define the types of elements at specific positions in the array.
+let user: [string, number] = ["Amandeep", 36]; // This is valid because user is a tuple that contains a string and a number.
