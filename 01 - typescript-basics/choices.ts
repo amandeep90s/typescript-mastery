bb8094c8dc37f5a47f952220b7fe419e2970d0b8
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-19 08:56:42 +0530

    type aliases and custom types

--- 01 - typescript-basics/choices.ts
+++ 01 - typescript-basics/choices.ts
@@ -8,6 +8,8 @@ const userRole: Role = Role.Editor;
 
 console.log(userRole); // Output: User role is: Editor
 
-const jobRole: "admin" | "editor" | "viewer" = "admin";
+type JobRole = "admin" | "editor" | "viewer";
+
+const jobRole: JobRole = "admin";
 
 console.log(jobRole); // Output: Job role is: admin
