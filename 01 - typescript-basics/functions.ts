851faddb61d460211a7632394286be47c33e9121
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-19 09:04:52 +0530

    the never type

--- 01 - typescript-basics/functions.ts
+++ 01 - typescript-basics/functions.ts
@@ -7,3 +7,10 @@ console.log(multiple(5, 10)); // Output: 50
 function log(message: string): void {
   console.log(message);
 }
+
+// never type function
+// never type is used to indicate that a function never returns a value and will always throw an error or terminate the program.
+// It is often used for functions that throw exceptions or have infinite loops.
+function throwError(message: string): never {
+  throw new Error(message);
+}
