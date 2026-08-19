3de16f10a9bc7eca2a141b93647369f6357f1349
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-19 08:40:14 +0530

    flexible objects with Record type

--- 01 - typescript-basics/objects.ts
+++ 01 - typescript-basics/objects.ts
@@ -22,3 +22,10 @@ val = 20;
 val = true;
 // val = null; // This will cause an error because null is not assignable to type '{}'
 // val = undefined; // This is allowed because undefined is assignable to type '{}'
+
+// Flexible objects with Record type
+const userRecord: Record<string, string | number> = {
+  name: "Jane Doe",
+  age: 25,
+  city: "New York",
+};
