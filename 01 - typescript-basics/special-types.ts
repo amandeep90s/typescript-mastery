610cdc11ccc1daf446741b5f7370ab19297cc037
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-19 09:23:45 +0530

    null & undefinded - special types

---
+++ 01 - typescript-basics/special-types.ts
@@ -0,0 +1,4 @@
+// null & undefined
+let nullValue: null = null; // nullValue can only be assigned null
+let undefinedValue: undefined | string = undefined; // undefinedValue can only be assigned undefined
+undefinedValue = "Hello"; // undefinedValue can also be assigned a string
