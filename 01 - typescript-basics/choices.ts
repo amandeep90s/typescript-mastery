939a8ec9ab3b4fa872cdec903a419593a106d8cd
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-08-19 08:53:08 +0530

    being specific with literal types

--- 01 - typescript-basics/choices.ts
+++ 01 - typescript-basics/choices.ts
@@ -7,3 +7,7 @@ enum Role {
 const userRole: Role = Role.Editor;
 
 console.log(userRole); // Output: User role is: Editor
+
+const jobRole: "admin" | "editor" | "viewer" = "admin";
+
+console.log(jobRole); // Output: Job role is: admin
